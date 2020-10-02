cbfab59c7e635522d1ba2f3e03c9c35a6949fc10
ewarren1994 <37820139+steelcity412@users.noreply.github.com>
2020-10-02 18:18:46 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BubbleSortSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 1, 4, 2, 8 };
+            int highestValue;
+
+            Console.WriteLine("Original array :");
+            
+            foreach (int elements in arr)
+            {
+                Console.Write(elements + " ");
+            }
+
+            for (int p = 0; p <= arr.Length - 2; p++)
+            {
+                for (int i = 0; i < arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        highestValue = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = highestValue;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n" + "Sorted array :");
+            
+            foreach (int elements in arr)
+            {
+                Console.Write(elements + " ");
+            }
+
+            Console.Write("\n");
+        
+        }
+    }
+}
